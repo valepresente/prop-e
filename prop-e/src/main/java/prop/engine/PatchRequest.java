729257d5917549a9e8ca802e60ec37f58a2245aa
Marcelo Manzan <manzan@gmail.com>
2014-11-04 00:19:00 -0200

    PatchRequest: loading PropOperation from JsonNode

--- prop-e/src/main/java/prop/engine/PatchRequest.java
+++ prop-e/src/main/java/prop/engine/PatchRequest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 public class PatchRequest {
 
@@ -15,7 +16,20 @@ public class PatchRequest {
 	}
 
 	private void loadOperations(JsonNode operations) {
-		// TODO: load operations from JsonNode
+		if (!operations.hasNonNull("operations")) return;
+		JsonNode node = operations.get("operations");
+		if (!node.isArray()) return;
+		ArrayNode list = (ArrayNode) node;
+		for (int n = 0, len = list.size(); n < len; n++) {
+			loadOperation(list.get(n));
+		}
+	}
+
+	private void loadOperation(JsonNode jsonNode) {
+		PropOperation operation = new PropOperation();
+		operation.setType(jsonNode.get("operationType").asText());
+		// TODO: load params
+		operations.add(operation);
 	}
 
 	public PatchResponse emptyResponse() {
--- prop-e/src/main/java/prop/engine/PropOperation.java
+++ prop-e/src/main/java/prop/engine/PropOperation.java
@@ -2,4 +2,13 @@ package prop.engine;
 
 public class PropOperation {
 
+	private String type;
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getType() {
+		return type;
+	}
+
 }
