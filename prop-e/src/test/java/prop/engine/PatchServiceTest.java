433c293aa64ce3236c1aef1caef9c9b95b02850b
Marcelo Manzan <manzan@gmail.com>
2014-11-13 23:39:05 -0200

    moving some tests to the right scope of class

--- prop-e/src/test/java/prop/engine/PatchServiceTest.java
+++ prop-e/src/test/java/prop/engine/PatchServiceTest.java
@@ -5,11 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.jsonassert.JsonAssert;
-
-import prop.core.patterns.cor.CORException;
 import prop.test.AbstractTestCase;
 
 public class PatchServiceTest extends AbstractTestCase {
@@ -20,44 +15,6 @@ public class PatchServiceTest extends AbstractTestCase {
 	@Test
 	public void testPatchServicePresence() {
 		assertNotNull(service);
-	}
-
-	@Test
-	public void testPretendCancelOrder() throws Exception {
-		JsonNode cancelOrderData = new ObjectMapper().readTree(this.getClass().getClassLoader()
-				.getResourceAsStream("prop/engine/fixtures/PatchService-pretendCancelOrder.json"));
-		PatchMessage message = new PatchMessage(cancelOrderData);
-		assertEquals(0, message.getEffectiveOperations().size());
-		service.pretend(message);
-		assertEquals(1, message.getEffectiveOperations().size());
-	}
-
-	@Test
-	public void testPretendWithEmptyJSON() throws Exception {
-		JsonNode emptyOrderData = new ObjectMapper().readTree("{}");
-		PatchMessage message = new PatchMessage(emptyOrderData);
-		try {
-			service.pretend(message);
-			assertTrue("Not reachable code", false);
-		} catch (CORException e) {
-			JsonAssert.with(message.getResponse().getErrors().toString())
-					.assertNotNull("$.message")
-					.assertEquals("$.message", "Missing operations");
-		}
-	}
-
-	@Test
-	public void testPretendWithInvalidOperationsType() throws Exception {
-		JsonNode emptyOrderData = new ObjectMapper().readTree("{\"operations\":{}}");
-		PatchMessage message = new PatchMessage(emptyOrderData);
-		try {
-			service.pretend(message);
-			assertTrue("Not reachable code", false);
-		} catch (CORException e) {
-			JsonAssert.with(message.getResponse().getErrors().toString())
-					.assertNotNull("$.message")
-					.assertEquals("$.message", "Invalid operations type");
-		}
 	}
 
 }
---
+++ prop-e/src/test/java/prop/engine/modes/PretendModeResolverTest.java
@@ -0,0 +1,64 @@
+package prop.engine.modes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import prop.core.patterns.cor.CORException;
+import prop.engine.PatchMessage;
+import prop.test.AbstractTestCase;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonassert.JsonAssert;
+
+public class PretendModeResolverTest extends AbstractTestCase {
+
+	@Autowired
+	private PretendModeResolver pretendResolver;
+
+	@Test
+	public void testCancelOrder() throws Exception {
+		JsonNode cancelOrderData = new ObjectMapper()
+				.readTree(this
+						.getClass()
+						.getClassLoader()
+						.getResourceAsStream(
+								"prop/engine/fixtures/PretendModeResolver-cancelOrder.json"));
+		PatchMessage message = new PatchMessage(cancelOrderData);
+		assertEquals(0, message.getEffectiveOperations().size());
+		pretendResolver.process(message);
+		assertEquals(1, message.getEffectiveOperations().size());
+	}
+
+	@Test
+	public void testWithEmptyJSON() throws Exception {
+		JsonNode emptyOrderData = new ObjectMapper().readTree("{}");
+		PatchMessage message = new PatchMessage(emptyOrderData);
+		try {
+			pretendResolver.process(message);
+			assertTrue("Not reachable code", false);
+		} catch (CORException e) {
+			JsonAssert.with(message.getResponse().getErrors().toString())
+					.assertNotNull("$.message")
+					.assertEquals("$.message", "Missing operations");
+		}
+	}
+
+	@Test
+	public void testWithInvalidOperationsType() throws Exception {
+		JsonNode emptyOrderData = new ObjectMapper()
+				.readTree("{\"operations\":{}}");
+		PatchMessage message = new PatchMessage(emptyOrderData);
+		try {
+			pretendResolver.process(message);
+			assertTrue("Not reachable code", false);
+		} catch (CORException e) {
+			JsonAssert.with(message.getResponse().getErrors().toString())
+					.assertNotNull("$.message")
+					.assertEquals("$.message", "Invalid operations type");
+		}
+	}
+
+}
