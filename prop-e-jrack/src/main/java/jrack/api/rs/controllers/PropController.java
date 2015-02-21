b2252ceb91430ae52f6ee01b1efeeb5832a9e907
Marcelo Manzan <manzan@gmail.com>
2015-02-20 22:56:07 -0200

    Removing old routes [ci skip]

--- prop-e-jrack/src/main/java/jrack/api/rs/controllers/PropController.java
+++ prop-e-jrack/src/main/java/jrack/api/rs/controllers/PropController.java
@@ -1,14 +1,10 @@
 package jrack.api.rs.controllers;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
@@ -34,22 +30,6 @@ abstract public class PropController<R extends PropRegistry> {
 
 	@Context
 	private UriInfo uriInfo;
-
-	@GET
-	@Path("")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response entrypoint() throws IOException {
-		InputStream in = this.getClass().getClassLoader()
-				.getResourceAsStream("jrack/api/rs/views/operations.json");
-		ObjectNode entity = (ObjectNode) new ObjectMapper().readTree(in);
-		for (String mode: getRegistry().getSupportedModes()) {
-			entity.with("_links").with(mode + "_operation_schema")
-					.put("href", "/operations/schemas/" + mode)
-					.put("type", "application/schema+json");
-		}
-		ResponseBuilder response = Response.ok(entity);
-		return response.build();
-	}
 
 	@GET
 	@Path("{id}/to/{op:[\\w]+}")
